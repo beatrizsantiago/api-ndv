1978628675aaa4e61063ccaf93637f416180b41d
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2020-08-15 15:20:45 -0300

    Fix details

--- WebApplication/ViewModels/Outputs/Life/DetailsLife.cs
+++ WebApplication/ViewModels/Outputs/Life/DetailsLife.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using WebApplication.ViewModels.Output.Account;
 using WebApplication.ViewModels.Output.Feedback;
 
@@ -7,13 +8,13 @@ namespace WebApplication.ViewModels.Output.Life
     public class DetailsLifeViewModel
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string FullName { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public DateTime Birthday { get; set; }
         public PreviewIntegratorViewModel Integrator { get; set; }
-        public PreviewFeedbackViewModel Feedbacks { get; set; }
-        public ProgressStepsViewModel HistoricPropheticWay { get; set; }
+        public List<PreviewFeedbackViewModel> Feedbacks { get; set; }
+        public List<ProgressStepsViewModel> HistoricPropheticWay { get; set; }
         
     }
 }
--- WebApplication/ViewModels/Outputs/Life/PreviewLifeViewModel.cs
+++ WebApplication/ViewModels/Outputs/Life/PreviewLifeViewModel.cs
@@ -3,7 +3,7 @@ namespace WebApplication.ViewModels.Output.Life
     public class PreviewLifeViewModel
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string FullName { get; set; }
         public string Phone { get; set; }
     }
 }
